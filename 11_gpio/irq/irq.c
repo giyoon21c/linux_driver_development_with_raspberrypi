049045c23bd83880cd941c17476d1af34584d59c
Madhawa Polkotuwa <madhawapolkotuwa@gmail.com>
2026-01-19 17:41:54 +0900

    GPIO Interrupts

--- 11_gpio/irq/irq.c
+++ 11_gpio/irq/irq.c
@@ -1,8 +1,16 @@
+#define USE_DESCRIPTOR_API 
+
 #include <linux/module.h>
 #include <linux/init.h>
-#include <linux/gpio.h>
+
+#ifdef USE_DESCRIPTOR_API 
+#include <linux/gpio/consumer.h>
+#else
+#include <linux/gpio.h> // legacy
+#endif
+
 #include <linux/interrupt.h>
-#include <linux/timer_types.h>
+#include <linux/timer_types.h> 
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("MPCoding - LDD");
@@ -18,6 +26,10 @@ static const char *device_name = "gpio_ctrl";
 static int led_gpio = (LED_GPIO + GPIO_OFFSET);
 static int button_gpio = (BUTTON_GPIO + GPIO_OFFSET);
 
+#ifdef USE_DESCRIPTOR_API
+static struct gpio_desc *led, *button;
+#endif
+
 /* Variable contains iqr number */
 unsigned int irq_number;
 
@@ -30,39 +42,79 @@ static struct timer_list debounce_timer;
 /* GPIO ISR */
 static irqreturn_t button_isr(int irq, void *dev_id){
     pr_info("%s: Interrupt occoured on GPIO 20\n", device_name);
+
+#ifdef USE_DESCRIPTOR_API
+        last_button_state = gpiod_get_value(button);
+#else
+        last_button_state = gpio_get_value(button_gpio);
+#endif
     
-    last_button_state = gpio_get_value(button_gpio);
     mod_timer(&debounce_timer, jiffies + msecs_to_jiffies(DEBOUNCE_DELAY));
     
     return IRQ_HANDLED;
 }
 
 static void debounce_timer_callback(struct timer_list *t){
+#ifdef USE_DESCRIPTOR_API
+    int state = gpiod_get_value(button);
+#else
     int state = gpio_get_value(button_gpio);
+#endif
     if(state == last_button_state){
         pr_info("%s: Valid button press detected\n", device_name);
+#ifdef USE_DESCRIPTOR_API
+        gpiod_set_value(led, !gpiod_get_value(led));
+#else
         gpio_set_value(led_gpio, !gpio_get_value(led_gpio));
+#endif
     }
 }
 
 static int __init my_init(void) {
     int status;
+
+#ifdef USE_DESCRIPTOR_API
+    led = gpio_to_desc(led_gpio);
+    if(!led){
+		pr_err("%s: unable gpio_to_desc led_gpio 21\n", device_name);
+		return -1;
+	}
+
+    button = gpio_to_desc(button_gpio);
+	if(!button){
+		pr_err("%s: unable gpio_to_desc button_gpio 20\n", device_name);
+		return -1;
+	}
+
+    status = gpiod_direction_output(led, 0);
+    if(status){
+        pr_err("%s: Failed to set gpio 20 led direction\n", device_name);
+        return -status;
+    }
+
+    status = gpiod_direction_input(button);
+    if(status){
+        pr_err("%s: Failed to set gpio 21 button direction\n", device_name);
+        return -status;
+    }
+
+#else
     status = gpio_request(led_gpio, "led_gpio");
     if(status){
         pr_err("%s: Failed to request led gpio 21\n", device_name);
         return -status;
     }
 
-    status = gpio_direction_output(led_gpio, 0);
+    status = gpio_request(button_gpio, "button_gpio");
     if(status){
-        pr_err("%s: Failed to set gpio 20 led direction\n", device_name);
+        pr_err("%s: Failed to request button gpio 20\n", device_name);
         gpio_free(led_gpio);
         return -status;
     }
 
-    status = gpio_request(button_gpio, "button_gpio");
+    status = gpio_direction_output(led_gpio, 0);
     if(status){
-        pr_err("%s: Failed to request button gpio 20\n", device_name);
+        pr_err("%s: Failed to set gpio 20 led direction\n", device_name);
         gpio_free(led_gpio);
         return -status;
     }
@@ -74,13 +126,23 @@ static int __init my_init(void) {
         gpio_free(button_gpio);
         return -status;
     }
-
+#endif
+      
+#ifdef USE_DESCRIPTOR_API
+    irq_number = gpiod_to_irq(button);
+#else
     irq_number = gpio_to_irq(button_gpio);
+#endif
+
     status = request_irq(irq_number, button_isr, IRQF_TRIGGER_FALLING, "btn_irq_handler", NULL);
     if(status){
         pr_err("%s: IRQ request failed\n", device_name);
+
+#ifndef USE_DESCRIPTOR_API
         gpio_free(led_gpio);
         gpio_free(button_gpio);
+ #endif
+
         return -status;
     }
     pr_info("%s: irq_number=%d \n", device_name, irq_number);
@@ -93,9 +155,13 @@ static int __init my_init(void) {
 }
 
 static void __exit my_exit(void){
+#ifdef USE_DESCRIPTOR_API
+    gpiod_set_value(led, 0);
+#else  
     gpio_set_value(led_gpio, 0);
     gpio_free(led_gpio);
     gpio_free(button_gpio);
+#endif
     free_irq(irq_number, NULL);
     del_timer_sync(&debounce_timer);
     pr_info("%s: Goodbye Kernel\n", device_name);
