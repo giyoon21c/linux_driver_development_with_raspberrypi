76751e9bf8abbc7546f88a4d2560d47a3e0a27d6
Your Name <you@example.com>
2026-03-03 08:09:13 -0800

    06 - character device driver - create and register character driver

--- 06_character_device_driver/01_my_first_cdev/my_cdev.c
+++ 06_character_device_driver/01_my_first_cdev/my_cdev.c
@@ -3,9 +3,11 @@
 #include <linux/fs.h>
 #include <linux/cdev.h>
 
+
+// hook in a module 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("MPCoding - LDD");
-MODULE_DESCRIPTION("Our first character device");
+MODULE_DESCRIPTION("Our first dynamically loadable kernel module");
 
 static const char *my_device = "my_cdev";
 
@@ -13,65 +15,43 @@ static dev_t dev_nr;
 
 static struct cdev my_cdev;
 
-static struct class *my_class;
-
+// file_operations struct
 static struct file_operations fops = {
 
 };
 
+// entry function 
 static int __init my_init(void) {
     int status;
-    status = alloc_chrdev_region(&dev_nr, 0, MINORMASK + 1, my_device);
-    if(status){
-        pr_err("%s: character device registation failed\n", my_device);
+
+    // new way
+    status = alloc_chrdev_region(&dev_nr, 0, MINORMASK + 1, my_device); 
+    if (status < 0) {
+        pr_err("%s: character device registraion failed\n", my_device);
         return status;
     }
 
+    // cdev_init
     cdev_init(&my_cdev, &fops);
     my_cdev.owner = THIS_MODULE;
-
+  
+    // add the device to kernel's device list
     status = cdev_add(&my_cdev, dev_nr, MINORMASK + 1);
-    if(status){
+    if (status) {
         pr_err("%s: error adding cdev\n", my_device);
-        goto free_device_nr;
+        unregister_chrdev_region(dev_nr, MINORMASK+1);
+        return status;
     }
 
-    my_class = class_create("my_class");
-    if(!my_class){
-        pr_err("%s: Could not create class my_class\n",my_device);
-        status = ENOMEM;
-        goto delete_cdev;
-    }
-
-    if(!device_create(my_class, NULL, dev_nr, NULL, "my_cdev%d", 0)){
-        pr_err("%s: Could not create device my_cdev0\n", my_device);
-        status = ENOMEM;
-        goto delete_class;
-    }
-
-    pr_info("%s: Caracter device registerd, Major number: %d Minor number: %d\n",my_device, MAJOR(dev_nr), MINOR(dev_nr));
-    pr_info("%s: Created device number under /sys/class/my_class\n", my_device);
-    pr_info("%s: Created new device node /dev/my_cdev\n", my_device);
+    pr_info("%s: Character device registered, Major number: %d Minor number: %d\n", 
+        my_device, MAJOR(dev_nr), MINOR(dev_nr)); 
     return 0;
-
-delete_class:
-    class_destroy(my_class);
-
-delete_cdev:
-    cdev_del(&my_cdev);
-
-free_device_nr:
-    unregister_chrdev_region(dev_nr, MINORMASK + 1); 
-
-    return status;
-
 }
 
+// exit function 
 static void __exit my_exit(void){
-    device_destroy(my_class, dev_nr);
-    class_destroy(my_class);
     cdev_del(&my_cdev);
-    unregister_chrdev_region(dev_nr, MINORMASK + 1);
+    unregister_chrdev_region(dev_nr, MINORMASK+1);
     pr_info("%s: Goodbye, Kernel\n", my_device);
 }
 
---
+++ 06_character_device_driver/01_my_first_cdev/my_cdev.mod.c
@@ -0,0 +1,33 @@
+#include <linux/module.h>
+#include <linux/export-internal.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__section(".gnu.linkonce.this_module") = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+
+
+static const struct modversion_info ____versions[]
+__used __section("__versions") = {
+	{ 0xe3ec2f2b, "alloc_chrdev_region" },
+	{ 0x92997ed8, "_printk" },
+	{ 0xa01f13a6, "cdev_init" },
+	{ 0x3a6d85d3, "cdev_add" },
+	{ 0x6091b333, "unregister_chrdev_region" },
+	{ 0x27271c6b, "cdev_del" },
+	{ 0x474e54d2, "module_layout" },
+};
+
+MODULE_INFO(depends, "");
+
+
+MODULE_INFO(srcversion, "26EBDD57BCE4DAE1C23A12C");
